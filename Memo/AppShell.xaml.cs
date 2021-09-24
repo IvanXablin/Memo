340e64cf38b33580dfd4019461391197f96a3b87
Ivan Xablin <amdandpc67889@gmail.com>
2021-09-24 15:13:55 +0700

    git init

---
+++ Memo/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Memo.Views;
+
+namespace Memo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(NoteEntryPage), typeof(NoteEntryPage));
+        }
+    }
+}
