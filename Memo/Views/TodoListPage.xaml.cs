321ed7faf4ac79c12a827bf61704280ba08b4627
Ivan Xablin <amdandpc67889@gmail.com>
2021-09-24 19:25:05 +0700

    git init

---
+++ Memo/Views/TodoListPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Memo.Data;
+using Memo.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Memo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoListPage : ContentPage
+    {
+        public TodoListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            listView.ItemsSource = await database.GetItemsAsync();
+        }
+
+        async void OnItemAdded(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new TodoItemPage
+                {
+                    BindingContext = e.SelectedItem as TodoItem
+                });
+            }
+        }
+    }
+}
